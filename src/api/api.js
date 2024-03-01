e5c52f9fd3ebd337cc252145fdcabce8859484bd
EddyBel <dante61918@gmail.com>
2024-03-01 16:51:50 -0600

    feat: Se agrego el prefijo /api para poder acceder a la api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = import.meta.env.VITE_API_URL;
+const API = `${import.meta.env.VITE_API_URL}/api`;
 
 export function getAboutMe() {
   return axios.get(`${API}/information`).then((response) => response.data);
